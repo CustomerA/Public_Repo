f93768b02f9ef469c6d9e81db6fb59fc3b72d74a
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2020-10-05 14:07:59 -0600

    Update oandaCopy.py

--- oandaCopy.py
+++ oandaCopy.py
@@ -13,3 +13,5 @@ oanda = opy.API(environment='practice',
                 access_token=my_oanda_access_credentials)
 
 awS_secret="7N1645LRTNT7PP8XX9E8M9C581EQ8PMP90P40P0K"
+
+awS_secret="7N1885LRTRM7PP8XX9E8M9C3F1EQ8PMP90P40P0K"
